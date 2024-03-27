8c9797e2239ee8d2585fe0c2cb8b3d5976ce87ae
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-03-27 16:58:13 +0700

    first commit

---
+++ lib/form_kontak.dart
@@ -0,0 +1,77 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class FormKontak extends StatefulWidget {
+  const FormKontak({super.key});
+
+  @override
+  State<FormKontak> createState() => _FormKontakState();
+}
+
+class _FormKontakState extends State<FormKontak> {
+  File? _image;
+  final _imagePicker = ImagePicker();
+
+  final _formKey = GlobalKey<FormState>();
+  final _namaController = TextEditingController();
+  final _alamatController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _noTeleponController = TextEditingController();
+
+  Future<void> getImage() async {
+    final XFile? pickedFile =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Input Data Kontak')),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Nama", hintText: "Masukkan Nama"),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Email", hintText: "Masukkan Email"),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "Alamat", hintText: "Masukkan Alamat"),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      labelText: "NoTelpon", hintText: "Masukkan NoTelpon"),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
